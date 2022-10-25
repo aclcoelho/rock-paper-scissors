04e9fe50dce3ae2b9adf07e5689da7d97adbe8b9
aclcoelho <ana.lunardello@gmail.com>
2022-10-25 00:10:54 -0300

    Altering conditional in script

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -28,33 +28,36 @@ scissors = '''
 '''
 
 choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if choice == 0:
-    print(rock)
-elif choice == 1:
-    print(paper)
-elif choice == 2:
-    print(scissors)
-
-print("Computer chose:")
-comp_choice = random.randint(0,2)
-if comp_choice == 0:
-    print(rock)
-elif comp_choice == 1:
-    print(paper)
-elif comp_choice == 2:
-    print(scissors)
-
-if choice == 0 and comp_choice == 2:
-    print("You win!")
-elif choice == 2 and comp_choice == 1:
-    print("You win!")
-elif choice == 1 and comp_choice == 0:
-    print("You win!")
-elif choice == 0 and comp_choice == 0:
-    print("It's a tie!")
-elif choice == 1 and comp_choice == 1:
-    print("It's a tie!")
-elif choice == 2 and comp_choice == 2:
-    print("It's a tie!")
+if choice >= 3 or choice < 0:
+    print("You typed an invalid number, you lose!")
 else:
-    print("You lose")
+    if choice == 0:
+        print(rock)
+    elif choice == 1:
+        print(paper)
+    elif choice == 2:
+        print(scissors)
+
+    print("Computer chose:")
+    comp_choice = random.randint(0,2)
+    if comp_choice == 0:
+        print(rock)
+    elif comp_choice == 1:
+        print(paper)
+    elif comp_choice == 2:
+        print(scissors)
+
+    if choice == 0 and comp_choice == 2:
+        print("You win!")
+    elif choice == 2 and comp_choice == 1:
+        print("You win!")
+    elif choice == 1 and comp_choice == 0:
+        print("You win!")
+    elif choice == 0 and comp_choice == 0:
+        print("It's a tie!")
+    elif choice == 1 and comp_choice == 1:
+        print("It's a tie!")
+    elif choice == 2 and comp_choice == 2:
+        print("It's a tie!")
+    else:
+        print("You lose")
